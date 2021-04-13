28609837889cf513877408cc8be1bfdfd9e2d76c
Izak <izak.jenko@icloud.com>
2021-04-13 15:10:19 +0200

    urejanje map

---
+++ DN2/Aux/izak_jenko_27181019/geffe.py
@@ -0,0 +1,80 @@
+alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+N = len(alphabet)
+
+to_num = {l : i for (l,i) in zip(list(alphabet),range(N))}
+to_char = alphabet
+
+def to_binary(plain_text):
+    """ Plaintext prevede v dvojiško zaporedje, kjer je vsaka črka predstavljena s 5 biti. Primer:
+    
+    D -> 3 -> 00011 """
+    binary = ''
+    for c in plain_text:
+        binary += '{0:05b}'.format(to_num[c])
+    return binary
+
+def to_plain_text(binary):
+    plain_text = ''
+    for i in range(0,len(binary),5):
+        plain_text += to_char[int(binary[i:i+5],2)]
+    return plain_text
+        
+
+def lfsr(poly, key):
+    """ poly ... seznam koeficientov polinoma \n
+        key ... ključ 
+    
+        ! Stopnja polinoma je enaka dolžini ključa !
+
+        list c = [c_0, c_1, ... , c_m] <-> polinom c_0 + c_1X + ... + c_mX^m
+
+        lfsr vrne generator, ki generira bite glede na karakteristični polinom predstavljen s seznamom poly:
+    
+        z_i = c[1]z[i - 1] + c[2]z[i - 2] + ... + c[m]z[i - m] (mod 2)
+
+        Prvih nekaj bitov predstavlja samo ključ.
+    """
+
+    m = len(key)
+    assert m == len(poly) - 1
+
+    # najprej vrne vse bite iz ključa
+    for k in key:
+        yield int(k)
+
+    while True:
+        next_bit = 0
+        for i in range(1,m + 1):
+            next_bit ^= poly[i]*int(key[m - i])
+
+        key = key[1:] + str(next_bit)
+        yield next_bit
+
+        
+def geffe(lfsr1, lfsr2, lfsr3):
+    while True:
+        x1 = next(lfsr1)
+        x2 = next(lfsr2)
+        x3 = next(lfsr3)
+        yield (x1 * x2) ^ (x2 * x3) ^ x3
+
+
+def encrypt(b, key, poly):
+    """ b ... besedilo podano v bitih kot string \n
+        key ... trojica ključev, ki so podani kot stringi bitov \n
+        poly ... trojica polinomov, ki podani kot seznam koeficientov, njihove stopnje so enake dolžinam pripadajočih ključev
+    """
+
+    lfsr1 = lfsr(poly[0], key[0])
+    lfsr2 = lfsr(poly[1], key[1])
+    lfsr3 = lfsr(poly[2], key[2])
+    g = geffe(lfsr1, lfsr2, lfsr3)
+
+    c = ''
+    for bit in b:
+        c += str(int(bit) ^ next(g))
+    return c
+
+def decrypt(c, key, poly):
+    # ker je +1 = -1 (mod 2) lahko uporabimo isto funkcijo
+    return encrypt(c, key, poly)
---
+++ DN2/Aux/izak_jenko_27181019/known_plaintext_attack.py
@@ -0,0 +1,74 @@
+import sys
+from geffe import to_binary, lfsr, geffe, decrypt, to_plain_text
+
+poly = (
+    [1, 0, 1, 0, 0, 1], 
+    [1, 1, 0, 0, 0, 0, 0, 1], 
+    [1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1]
+    )
+
+key_length = tuple(len(p) - 1 for p in poly)
+
+
+def index_of_coincidence(text1,text2):
+    assert len(text1) == len(text2)
+
+    counter = 0
+    for (t1, t2) in zip(text1,text2):
+        if t1 == t2:
+            counter += 1
+    return counter / len(text1)
+
+
+def find_matching_key(i, z):
+    for k in range(2**key_length[i] - 1):
+        k = f'{k:0b}'.zfill(key_length[i])
+        lfsr1 = lfsr(poly[i], k)
+
+        y = ''.join(str(next(lfsr1)) for _ in range(len(z)))
+
+        if index_of_coincidence(y, z) >= 0.75:
+            return k
+
+
+def known_plaintext_attack(plaintext, ciphertext):
+    """ plaintext, ciphertext ... podana kot stringa bitov iste dolžine. \n
+        Ta funkcija poišče ključ ki povezuje kriptogram in besedilo na osnovi korelacijskega napada.
+    """
+    n = len(plaintext)
+
+    assert n == len(ciphertext)
+
+    z = '{0:0b}'.format(int(plaintext, 2) ^ int(ciphertext, 2)).zfill(n)
+
+    fst_key = find_matching_key(0, z)
+    trd_key = find_matching_key(2, z)
+  
+    for k in range(2**key_length[1] - 1):
+        k = f'{k:0b}'.zfill(key_length[1])
+
+        g = geffe(
+            lfsr(poly[0], fst_key),
+            lfsr(poly[1], k),
+            lfsr(poly[2], trd_key)
+            )
+
+        y = ''.join(str(next(g)) for _ in range(n))
+
+        if z == y:
+            return fst_key, k, trd_key
+
+##### SKRIPTA #####
+
+known_plaintext = to_binary('CRYPTOGRAPHY')
+
+ciphertext = sys.stdin.read()
+
+key = known_plaintext_attack(known_plaintext, ciphertext[:len(known_plaintext)])
+
+print(key)
+
+# plaintext = '000101000111000011111001101110001101000100000011110011111000'
+# ciphertext = '011001110111111110011000111110101010111001111011000111111001'
+
+print(to_plain_text(decrypt(ciphertext, key, poly)))
